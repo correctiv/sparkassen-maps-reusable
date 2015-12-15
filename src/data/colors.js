120d1b400e60a987e8b77acd845c29f3649f9fc2
Simon Wörpel <simon.woerpel@medienrevolte.de>
2015-12-15 16:52:29 +0100

    Painting

--- src/data/colors.js
+++ src/data/colors.js
@@ -1,3 +1,10 @@
-const COLORS = ['#f7fcfd','#e5f5f9','#ccece6','#99d8c9','#66c2a4','#41ae76','#238b45','#006d2c','#00441b']
+// http://colorbrewer2.org/
+//
+// const COLORS = ['#f7fcfd','#e5f5f9','#ccece6','#99d8c9','#66c2a4','#41ae76','#238b45','#006d2c','#00441b']
+
+// const COLORS = ['#d73027','#f46d43','#fdae61','#fee08b','#ffffbf','#d9ef8b','#a6d96a','#66bd63','#1a9850']
+
+// http://colorbrewer2.org/?type=diverging&scheme=RdYlBu&n=9
+const COLORS = ['#d73027','#f46d43','#fdae61','#fee090','#e0f3f8','#abd9e9','#74add1','#4575b4'].reverse()
 
 export default COLORS
