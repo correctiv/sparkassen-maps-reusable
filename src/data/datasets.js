a658648fb52f526493270389c15b1bbdfa7a0836
Simon Wörpel <simon.woerpel@medienrevolte.de>
2015-12-15 18:37:17 +0100

    Update datasets

--- src/data/datasets.js
+++ src/data/datasets.js
@@ -6,50 +6,74 @@ const SOURCES = [
     shortName: 'E. coli',
     name: 'Escherichia coli',
     title: 'Percentage with resistance to fluoroquinolones (%R)',
-    data: require('dsv!./csv/data3.csv')
+    annotation: 'Data: 2014, except Poland: 2013',
+    euData: 22.4,
+    data: require('dsv!./csv/0_e-coli.csv')
   },
   {
-    name: 'Keim 1',
-    data: require('dsv!./csv/data.csv')
+    shortName: 'E. coli 3rd',
+    name: 'Escherichia coli',
+    title: 'Percentage with resistance to third-generation cephalosporins (%R)',
+    annotation: 'Data: 2014, except Poland: 2013',
+    euData: 12,
+    data: require('dsv!./csv/1_e-coli.csv')
   },
   {
-    name: 'Keim 2',
-    data: require('dsv!./csv/data2.csv')
+    shortName: 'K. p. 3rd',
+    name: 'Klebsiella pneumoniae',
+    title: 'Percentage with resistance to third-generation cephalosporins (%R)',
+    euData: 28,
+    annotation: 'Data: 2014, except Poland: 2013',
+    data: require('dsv!./csv/2_k-p.csv')
   },
   {
-    name: 'Keim 1',
-    data: require('dsv!./csv/data.csv')
+    shortName: 'K. p.',
+    name: 'Klebsiella pneumoniae',
+    title: 'Percentage with resistance to carbapenems (%R)',
+    euData: 7.3,
+    annotation: 'Data: 2014, except Poland: 2013',
+    data: require('dsv!./csv/3_k-p.csv')
   },
   {
-    name: 'Keim 1',
-    data: require('dsv!./csv/data.csv')
+    shortName: 'S. p.',
+    name: 'Streptococcus pneumoniae',
+    title: 'Percentages non-susceptible to penicillin (%IR)',
+    annotation: 'Data: 2014, except Poland: 2013',
+    data: require('dsv!./csv/4_s-p.csv')
   },
   {
-    name: 'Keim 1',
-    data: require('dsv!./csv/data.csv')
+    shortName: 'MRSA',
+    name: 'Staphylococcus aureus',
+    title: 'Percentage with resistance to meticillin (MRSA) (%R)',
+    euData: 17.4,
+    annotation: 'Data: 2014, except Poland: 2013',
+    data: require('dsv!./csv/5_mrsa.csv')
   },
   {
-    name: 'Keim 1',
-    data: require('dsv!./csv/data.csv')
+    shortName: 'NTS',
+    name: 'NTS',
+    annotation: 'Source: WHO',
+    data: require('dsv!./csv/6_nts.csv')
   },
   {
-    name: 'Keim 1',
-    data: require('dsv!./csv/data.csv')
+    shortName: 'Shg.',
+    name: 'Shigella',
+    annotation: 'Source: WHO',
+    data: require('dsv!./csv/7_shigella.csv')
   },
   {
-    name: 'Keim 2',
-    data: require('dsv!./csv/data2.csv')
+    shortName: 'Neiss.',
+    name: 'Neisseria',
+    data: require('dsv!./csv/8_neisseria.csv')
   }
 ]
 
 let DATASETS = []
 for (let source of SOURCES) {
-  let data = source.data,
-    name = source.name,
-    shortName = source.shortName || source.name,
-    title = source.title
+  let data = source.data
   let loader = new Loader({colors, data})
-  let metaData = {name, shortName, title}
+  delete source.data
+  let metaData = source
   let dataset = {metaData, loader}
   DATASETS.push(dataset)
 }
