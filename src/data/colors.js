d98fb72d00bfa5043b247f6a4acfdc7fcec58edf
Simon Jockers <hello@simonjockers.com>
2015-12-17 13:06:33 +0100

    Update colors.

--- src/data/colors.js
+++ src/data/colors.js
@@ -1,10 +1,6 @@
 // http://colorbrewer2.org/
 //
-// const COLORS = ['#f7fcfd','#e5f5f9','#ccece6','#99d8c9','#66c2a4','#41ae76','#238b45','#006d2c','#00441b']
-
-// const COLORS = ['#d73027','#f46d43','#fdae61','#fee08b','#ffffbf','#d9ef8b','#a6d96a','#66bd63','#1a9850']
-
-// http://colorbrewer2.org/?type=diverging&scheme=RdYlBu&n=9
-const COLORS = ['#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#99000d'].reverse()
+const COLORS = ['#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#99000d']
 
 export default COLORS
+
